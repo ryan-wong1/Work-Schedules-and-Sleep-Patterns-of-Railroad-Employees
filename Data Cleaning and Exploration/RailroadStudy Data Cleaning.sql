0ea00a4d886ec3ca341337bd233682fa39e89809
Ryan Wong <72681183+ryan-wong1@users.noreply.github.com>
2024-12-18 10:39:49 -0600

    Update RailroadStudy Data Cleaning.sql

--- Data Cleaning and Exploration/RailroadStudy Data Cleaning.sql
+++ Data Cleaning and Exploration/RailroadStudy Data Cleaning.sql
@@ -5,7 +5,13 @@ use RailroadStudy;
 select *
 from dbo.RailroadData;
 
--- create duplicate railroaddata table
+-- duplicate copy of RailroadData:
+--select *
+--into dbo.RailroadData1
+--from dbo.RailroadData;
+
+select *
+from dbo.RailroadData1;
 
 select *
 from dbo.Variables;
@@ -36,77 +42,167 @@ Inadeq_Staff, Resp_for_others_safety, BreakTime, TimeOff:
 
 --Age_Group: 1=20-29, 2=30-39, 3=40-49, 4=50-59, 5=60+
 select Age_Group
-from dbo.RailroadData;
+from dbo.RailroadData1;
 
 select age_group,
 case
-when Age_Group = 1 then '20-29'
-when Age_Group = 2 then '30-39'
-when Age_Group = 3 then '40-49'
-when Age_Group = 4 then '50-59'
-when Age_Group = 5 then '60+'
+when age_group = 1 then '20-29'
+when age_group = 2 then '30-39'
+when age_group = 3 then '40-49'
+when age_group = 4 then '50-59'
+when age_group = 5 then '60+'
 end
-from dbo.RailroadData;
+from dbo.RailroadData1;
+
 
 --Sex: 1=Male, 2=Female
 select Sex
-from dbo.RailroadData;
+from dbo.RailroadData1;
+
+select sex,
+case
+when sex = 1 then 'Male'
+when sex = 2 then 'Female'
+end
+from dbo.RailroadData1;
 
 
 --Job_type: 1=trick, 2=asst chief
 select Job_type
-from dbo.RailroadData;
+from dbo.RailroadData1;
+
+select job_type,
+case
+when job_type = 1 then 'Trick Dispatcher'
+when job_type = 2 then 'Assistant Chief'
+end
+from dbo.RailroadData1;
 
 
 --Marital_Status: 1=Not married, 2=married
 select Marital_Status
-from dbo.RailroadData;
+from dbo.RailroadData1;
+
+select marital_status,
+case
+when marital_status = 1 then 'Not married'
+when marital_status = 2 then 'Married'
+end
+from dbo.RailroadData1;
 
 
 --Caff_Beverages: 1=yes, 2=no
 select Caff_Beverages
-from dbo.RailroadData;
+from dbo.RailroadData1;
+
+select caff_beverages,
+case
+when caff_beverages = 1 then 'Yes'
+when caff_beverages = 2 then 'No'
+end
+from dbo.RailroadData1;
 
 
 --Health_status: 1=poor, 2=fair, 3=good, 4=excellent
 select Health_status
-from dbo.RailroadData;
+from dbo.RailroadData1;
+
+select health_status,
+case
+when health_status = 1 then 'Poor'
+when health_status = 2 then 'Fair'
+when health_status = 3 then 'Good'
+when health_status = 4 then 'Excellent'
+end
+from dbo.RailroadData1;
 
 
 --Older_Younger_or_Same: 1=older, -1=younger, 0=same
 select Older_Younger_or_Same
-from dbo.RailroadData;
+from dbo.RailroadData1;
+
+select older_younger_or_same,
+case
+when older_younger_or_same = 1 then 'Older'
+when older_younger_or_same = -1 then 'Younger'
+when older_younger_or_same = 0 then 'Same'
+end
+from dbo.RailroadData1;
 
 
 --Diagnosed_Sleep_disorder: 1=yes, 2=no
 select Diagnosed_Sleep_disorder
-from dbo.RailroadData;
+from dbo.RailroadData1;
+
+select diagnosed_sleep_disorder,
+case
+when diagnosed_sleep_disorder = 1 then 'Yes'
+when diagnosed_sleep_disorder = 2 then 'No'
+end
+from dbo.RailroadData1;
 
 
 --Sleep_Apnea: 1=yes, 2=no
 select Sleep_Apnea
-from dbo.RailroadData;
+from dbo.RailroadData1;
+
+select sleep_apnea,
+case
+when sleep_apnea = 1 then 'Yes'
+when sleep_apnea = 2 then 'No'
+end
+from dbo.RailroadData1;
 
 
 --Medical_treatment: 1=yes, 2=no, all blanks are N/A
 select Medical_treatment
-from dbo.RailroadData;
+from dbo.RailroadData1;
+
+select medical_treatment,
+case
+when medical_treatment = 1 then 'Yes'
+when medical_treatment = 2 then 'No'
+end
+from dbo.RailroadData1;
 
 
 --job_schedule: 1=1st, 2=2nd, 3=3rd, 4=relief, 5=extraboard
 select job_schedule
-from dbo.RailroadData;
+from dbo.RailroadData1;
+
+select job_schedule,
+case
+when job_schedule = 1 then '1st'
+when job_schedule = 2 then '2nd'
+when job_schedule = 3 then '3rd'
+when job_schedule = 4 then 'Relief'
+when job_schedule = 5 then 'Extraboard'
+end
+from dbo.RailroadData1;
 
 
 --**Relief_schedule: e.g., 11223 = 1st shift, 1st shift, 2nd shift, 2nd shift, 3rd shift
 select Relief_schedule
-from dbo.RailroadData;
+from dbo.RailroadData1;
+
+select relief_schedule,
+case
+when relief_schedule = 1 then ''
+when relief_schedule = 1 then ''
+end
+from dbo.RailroadData1;
 
 
 --FRA_report: 1=yes, 2=no
 select FRA_report
-from dbo.RailroadData;
+from dbo.RailroadData1;
 
+select fra_report,
+case
+when fra_report = 1 then 'Yes'
+when fra_report = 2 then 'No'
+end
+from dbo.RailroadData1;
 
 
 
